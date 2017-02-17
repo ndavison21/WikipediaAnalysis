d9e64bedd647bc2f9f79d793534b8f48b2803cea
Nathanael Davison <natemail@mail.com>
2017-02-17 11:42:00 +0000

    Clustering and max value

--- wikipedia/clustering.py
+++ wikipedia/clustering.py
@@ -7,4 +7,35 @@ from scipy import optimize
 
 print "Reading in Graph."
 g = nx.read_edgelist('wiki-Talk.txt', create_using=nx.DiGraph(), nodetype=int)
-print "Graph Imported, analysing and plotting node degrees."
+print "Graph Imported, analysing and plotting node clustering coefficients."
+
+avg_clustering = nx.average_clustering(g)
+clusterings = nx.clustering(g)
+values = sorted(sorted(set(in_degrees.values())))
+hist = [clusterings.values().count(x) for x in values]
+
+print "Drawing Clusterings (linear scale)"
+plt.figure()
+plt.grid(True)
+plt.loglog(values, hist, 'ro-')
+plt.legend(['Clustering Coefficient'])
+plt.xlabel('Clustering Coefficient')
+plt.ylabel('Number of Nodes')
+plt.title('Wikipedia Talk Network')
+plt.xlim([0, max(values)])
+plt.savefig('results/log_clustering_coefficient.pdf')
+plt.close()
+
+print "Drawing Clusterings (log scale)"
+plt.figure()
+plt.grid(True)
+plt.loglog(values, hist, 'ro-')
+plt.legend(['Clustering Coefficient'])
+plt.xlabel('Clustering Coefficient')
+plt.ylabel('Number of Nodes')
+plt.title('Wikipedia Talk Network')
+plt.xlim([0, max(values)])
+plt.savefig('results/log_clustering_coefficient.pdf')
+plt.close()
+
+print "We Done Here."
