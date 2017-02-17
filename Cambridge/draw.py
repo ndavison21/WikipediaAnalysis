6b36c27a71d625a10278287c58b89d4436e69045
Nathanael Davison <natemail@mail.com>
2017-02-17 18:17:18 +0000

    Testing on Cam Network

---
+++ Cambridge/draw.py
@@ -0,0 +1,16 @@
+print "Importing Libraries."
+import networkx as nx
+import pylab as plt
+
+print "Reading in Graph."
+g = nx.read_edgelist('cambridge_net.txt', create_using=nx.DiGraph(), nodetype=int)
+
+print "Performing Spring Layout."
+spring_pos = nx.spring_layout(g)
+
+print "Drawing Network"
+plt.axis("off")
+nx.draw_networkx(g, pos = spring_pos, with_labels = False, node_size = 35)
+print "Saving Figure"
+plt.savefig('cam.pdf')
+plt.close()
