17baffff4ea17c6f6d1fe04a75fc3c28434f2313
Nathanael Davison <natemail@mail.com>
2017-02-20 09:45:03 +0000

    Betweeness and drawing

--- Cambridge/draw.py
+++ Cambridge/draw.py
@@ -1,6 +1,7 @@
 print "Importing Libraries."
 import networkx as nx
 import matplotlib
+from networkx.drawing.nx_agraph import graphviz_layout
 
 matplotlib.use('AGG')
 
@@ -11,12 +12,14 @@ plt.ioff()
 print "Reading in Graph."
 g = nx.read_edgelist('cambridge_net.txt', create_using=nx.DiGraph(), nodetype=int)
 
-print "Performing Spring Layout."
-spring_pos = nx.spring_layout(g)
-
-print "Drawing Network"
+print "Drawing Network (Spring)"
 plt.axis("off")
-nx.draw_networkx(g, pos = spring_pos, with_labels = False, node_size = 35)
+nx.draw_spring(g, with_labels = False, node_size = 35)
 print "Saving Figure"
-plt.savefig('results/cam.pdf')
+plt.savefig('results/cam_spring.pdf')
+
+print "Drawing Network (Spectral)"
+nx.draw_spectral(g, with_labels = False, node_size = 35)
+print "Saving Figure"
+plt.savefig('results/cam_spectral.pdf')
 plt.close()
