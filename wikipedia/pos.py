b2ae05ae016d57d31aa51d62b3ba2c61d03e5449
Nathanael Davison <natemail@mail.com>
2017-02-20 20:48:43 +0000

    pos generate and write to file

---
+++ wikipedia/pos.py
@@ -0,0 +1,30 @@
+import sys
+print "Importing Libraries."
+sys.stdout.flush()
+import networkx as nx
+import matplotlib
+import pickle
+
+matplotlib.use('AGG')
+
+import matplotlib.pyplot as plt
+
+plt.ioff()
+
+print "Reading in 100 Graph."
+sys.stdout.flush()
+g = nx.read_edgelist('data/wiki-Talk_100.txt', create_using=nx.DiGraph(), nodetype=int)
+
+print "Drawing Network (50)"
+spring_pos = nx.spring_layout(g, iterations = 50)
+print "Saving Positions"
+sys.stdout.flush()
+with open('spring_pos_50.pickle', 'wb+') as handle:
+    pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+print "Drawing Network (100)"
+spring_pos = nx.spring_layout(g, iterations = 100)
+print "Saving Positions"
+sys.stdout.flush()
+with open('spring_pos_100.pickle', 'wb+') as handle:
+    pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
