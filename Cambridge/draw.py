2a9048bed0cc555069004412b04c1e5927071b18
Nathanael Davison <natemail@mail.com>
2017-02-21 09:21:09 +0000

    Cam draw/pos fixes

--- Cambridge/draw.py
+++ Cambridge/draw.py
@@ -1,6 +1,7 @@
 print "Importing Libraries."
 import networkx as nx
 import matplotlib
+import pickle
 
 matplotlib.use('AGG')
 
@@ -8,11 +9,15 @@ import matplotlib.pyplot as plt
 
 plt.ioff()
 
-with open('spring_pos_50.pickle', 'rb') as handle:
+print "Reading in Graph"
+g = nx.read_edgelist('cambridge_net.txt', create_using=nx.DiGraph(), nodetype=int)
+
+
+with open('spring_pos_100.pickle', 'rb') as handle:
     spring_pos = pickle.load(handle)
 
 print "Drawing Network (Spring)"
 plt.axis("off")
-nx.draw_spring(g, spring_pos, with_labels = False, node_size = 35)
+nx.draw(g, spring_pos, with_labels = False, node_size = 35)
 print "Saving Figure"
 plt.savefig('results/cam_spring.pdf')
