e6b9c23cf89a6735ad15e29614c7e7de9b77b2e6
Nathanael Davison <natemail@mail.com>
2017-02-21 09:17:43 +0000

    Cam draw from pos

--- Cambridge/draw.py
+++ Cambridge/draw.py
@@ -8,17 +8,11 @@ import matplotlib.pyplot as plt
 
 plt.ioff()
 
-print "Reading in Graph."
-g = nx.read_edgelist('cambridge_net.txt', create_using=nx.DiGraph(), nodetype=int)
+with open('spring_pos_50.pickle', 'rb') as handle:
+    spring_pos = pickle.load(handle)
 
 print "Drawing Network (Spring)"
 plt.axis("off")
-nx.draw_spring(g, with_labels = False, node_size = 35)
+nx.draw_spring(g, spring_pos, with_labels = False, node_size = 35)
 print "Saving Figure"
 plt.savefig('results/cam_spring.pdf')
-
-print "Drawing Network (Spectral)"
-nx.draw_spectral(g, with_labels = False, node_size = 35)
-print "Saving Figure"
-plt.savefig('results/cam_spectral.pdf')
-plt.close()
