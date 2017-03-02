c905921e8f9c54fb0409da5f96a816519eae0696
ndavison21 <natemail@mail.com>
2017-03-02 09:42:14 +0000

    merge

--- wikipedia/sample_node_ranking.py
+++ wikipedia/sample_node_ranking.py
@@ -7,43 +7,20 @@ def get_top_keys(dictionary, top):
 	items.sort(reverse=True, key=lambda x: x[1])
 	return items[:top]
 
-print "Reading in Sample Graph."
-sys.stdout.flush()
-g = nx.read_edgelist('data/wiki-Talk_rw.txt', create_using=nx.DiGraph(), nodetype=int)
-print "Graph Imported, analysing and plotting node degrees."
+g = nx.read_edgelist('data/sample_giant_component.txt', create_using=nx.Graph(), nodetype=int)
 
-print "Convert to undirected graph."
-sys.stdout.flush()
-g_ud = g.to_undirected()
-
-file = open("results/sample/basic_info.txt", "w+")
-
-file.write("*** Sample Graph ***\n")
-print "Basic Graph Info."
-sys.stdout.flush()
-file.write(nx.info(g))
-
-print "Reciprocity."
-sys.stdout.flush()
-reciprocated = 0
-for (u,v) in g.edges_iter():
-	if g.has_edge(v, u):
-		reciprocated = reciprocated + 1
-file.write("\nReciprocity: {}\n".format(float(reciprocated)/nx.number_of_edges(g)))
-
-file.close()
 file = open("results/sample/ranking_nodes.txt", "w+")
 
 print "Ranking Nodes"
 file.write("\n*** RANKING NODES ***\n")
-file.write("Top 20 nodes by In-Edits\n")
-print "In-Edits."
-sys.stdout.flush()
-in_degree_centrality = nx.in_degree_centrality(g)
-file.write("Node ID, Centrality\n")
-for node in get_top_keys(in_degree_centrality, 20):
-	file.write("{}, {}\n".format(node[0], node[1]))
-file.flush()
+# file.write("Top 20 nodes by In-Edits\n")
+# print "In-Edits."
+# sys.stdout.flush()
+# in_degree_centrality = nx.in_degree_centrality(g)
+# file.write("Node ID, Centrality\n")
+# for node in get_top_keys(in_degree_centrality, 20):
+# 	file.write("{}, {}\n".format(node[0], node[1]))
+# file.flush()
 
 print "Betweenness."
 sys.stdout.flush()
