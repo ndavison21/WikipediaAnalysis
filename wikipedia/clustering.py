1edb577067211b1b52de1ae6a47fc5f9b68cf465
Nathanael Davison <natemail@mail.com>
2017-03-06 22:28:13 +0000

    Memory Optimisation

--- wikipedia/clustering.py
+++ wikipedia/clustering.py
@@ -2,54 +2,84 @@ print "Importing Libraries"
 
 import networkx as nx
 from multiprocessing import Pool
+from functools import partial
 from sys import stdout
 from math import floor
 from numpy import histogram
-
-print "Reading in Graph."
-stdout.flush()
-g = nx.read_edgelist('data/wik-Talk.txt', create_using=nx.Graph(), nodetype=int)
-
-def clust_fun(sources):
-	return nx.clustering(g, sources)
-
-p = Pool()
-num_partitions = len(p._pool)
-nodes = g.nodes()
-size_partitions = int(floor(len(nodes) / num_partitions))
-
-print num_partitions, "partitions of size", size_partitions
-stdout.flush()
-
-partitions = list()
-for i in range(0, num_partitions):
-	partitions.append(nodes[i*size_partitions:(i+1)*size_partitions-1])
+import itertools
 
 
+def partitions(nodes, n):
+    nodes_iter = iter(nodes)
+    while True:
+        partition = tuple(itertools.islice(nodes_iter,n))
+        if not partition:
+            return
+        yield partition
 
-clusts = p.map(clust_fun, partitions)
-clust = clusts[0]
+
+if __name__ == '__main__':
+
+	print "Reading in Graph."
+	stdout.flush()
+	g = nx.read_edgelist('wiki-Talk.txt', create_using=nx.Graph(), nodetype=int)
+
+	clust_fun = partial(nx.clustering, g)
+
+	p = Pool()
+	num_partitions = len(p._pool)
+	size_partitions = int(len(g) / num_partitions)
+
+	print num_partitions, "partitions of size", size_partitions
+	stdout.flush()
+
+	npartitions = list(partitions(g.nodes(), size_partitions))
 
 
-for i in range(1,num_partitions):
-	for n, nc in clusts[i].items():
-		clust[n] = nc
 
-print "Writing node clutsering to file"
-stdout.flush()
-with open("data.node_clustering.txt", "w+") as file:
-	for n, nc in clust.iteritems():
-		file.write("{} {}".format(n, nc))
+	clusts = p.imap(clust_fun, npartitions)
+	clust = dict()
 
-print "Creating Histogram"
-stdout.flush()
-hist = histogram(clust.values(), bins=100)
+	for c in clusts:
+		for n, nc in c.iteritems():
+			clust[n] = nc
 
-with open("results/clustering_hist.txt", "w+") as file:
-	h, e  = hist
-	file.write("Values\n")
-	for i in h:
-		file.write("{}\n".format(i))
-	file.write("Edges\n")
-	for i in e:
-		file.write("{}\n".format(i))
+	print "Writing node clutsering to file"
+	stdout.flush()
+	with open("data/node_clustering.txt", "w+") as file:
+		for n, nc in clust.iteritems():
+			file.write("{} {}\n".format(n, nc))
+
+	print "Creating Histograms"
+	stdout.flush()
+
+	hist = histogram(clust.values(), bins=1000)
+	with open("results/clustering_hist_1000.txt", "w+") as file:
+		h, e  = hist
+		file.write("Values\n")
+		for i in h:
+			file.write("{}\n".format(i))
+		file.write("Edges\n")
+		for i in e:
+			file.write("{}\n".format(i))
+
+	
+	hist = histogram(clust.values(), bins=10000)
+	with open("results/clustering_hist_10000.txt", "w+") as file:
+		h, e  = hist
+		file.write("Values\n")
+		for i in h:
+			file.write("{}\n".format(i))
+		file.write("Edges\n")
+		for i in e:
+			file.write("{}\n".format(i))
+
+	hist = histogram(clust.values(), bins=100000)
+	with open("results/clustering_hist_100000.txt", "w+") as file:
+		h, e  = hist
+		file.write("Values\n")
+		for i in h:
+			file.write("{}\n".format(i))
+		file.write("Edges\n")
+		for i in e:
+			file.write("{}\n".format(i))
