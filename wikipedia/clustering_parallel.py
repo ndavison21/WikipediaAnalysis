bae39005db8336f6e74bf4207b52de0a77c4ada2
ndavison21 <natemail@mail.com>
2017-03-07 10:34:35 +0000

    debug statements; another clustering attempt

--- wikipedia/clustering_parallel.py
+++ wikipedia/clustering_parallel.py
@@ -14,49 +14,45 @@ def chunks(l, n):
         yield x
 
 
-def _betmap(G_normalized_weight_sources_tuple):
+def _clustmap(G_normalized_weight_sources_tuple):
     """Pool for multiprocess only accepts functions with one argument.
     This function uses a tuple as its only argument. We use a named tuple for
     python 3 compatibility, and then unpack it when we send it to
     `betweenness_centrality_source`
     """
-    return nx.betweenness_centrality_source(*G_normalized_weight_sources_tuple)
+    return nx.clustering(*G_normalized_weight_sources_tuple)
 
 
-def betweenness_centrality_parallel(G, processes=None):
-    """Parallel betweenness centrality  function"""
+def clustering_parallel(G, processes=None):
+    """Parallel clustering  function"""
     p = Pool(processes=processes)
     node_divisor = len(p._pool)*4
     node_chunks = list(chunks(G.nodes(), int(G.order()/node_divisor)))
     num_chunks = len(node_chunks)
-    print "Mapping {} chunks to {} processes".format(num_chunks, len(p._pool))
-    stdout.flush()
-    bt_sc = p.map(_betmap,
+    cl_sc = p.map(_clustmap,
                   zip([G]*num_chunks,
-                      [True]*num_chunks,
-                      [None]*num_chunks,
-                      node_chunks))
-    print "Reducing partial solutions"
-    stdout.flush()
+                      node_chunks,
+                      [None]*num_chunks))
+
     # Reduce the partial solutions
-    bt_c = bt_sc[0]
-    for bt in bt_sc[1:]:
-        for n in bt:
-            bt_c[n] += bt[n]
-    return bt_c
+    cl_c = cl_sc[0]
+    for cl in cl_sc[1:]:
+        for n in cl:
+            cl_c[n] += cl[n]
+    return cl_c
 
 if __name__ == "__main__":
     print "Reading in Graph"
     stdout.flush()
-    g = nx.read_edgelist('data/wiki-Talk.txt', create_using=nx.DiGraph(), nodetype=int)
+    g = nx.read_edgelist('data/wiki-Talk.txt', create_using=nx.Graph(), nodetype=int)
 
-    print("Computing betweenness centrality for:")
+    print("Computing clustering for:")
     print(nx.info(g))
     print("Parallel version")
     stdout.flush()
-    bt = betweenness_centrality_parallel(g)
+    cl = clustering_parallel(g)
 
-    with open("results/node_betweenness.txt", "w+") as file:
+    with open("results/node_clustering.txt", "w+") as file:
         for n, b in bt.iteritems():
             file.write("{} {}\n".format(n, b))
 
