4a70a6820b836d3779ac585208013e4305f686f6
ndavison21 <natemail@mail.com>
2017-03-07 17:51:03 +0000

    restructuring results

--- wikipedia/degree_n.py
+++ wikipedia/degree_n.py
@@ -1,16 +1,45 @@
 print "Importing Libraries"
 
 import networkx as nx
-from sys import stdout
+import sys
+
+def get_top_keys(dictionary, size):
+    min_node = -1
+    min_value = sys.maxint
+    top = dict()
+    for (n, b) in dictionary.iteritems():
+        if len(top) < size:
+            top[n] = b
+            if b < min_value:
+                min_node = n
+                min_value = b
+        elif b > min_value:
+            del top[min_node]
+            top[n] = b
+            min_value = sys.maxint
+            for (m, c) in top.iteritems():
+                if c < min_value:
+                    min_node = m
+                    min_value = c
+
+    items = sorted(top.iteritems(), key=lambda (k,v): (v,k), reverse=True)
+
+    return items
 
 print "Reading in Graph."
 g = nx.read_edgelist('data/wiki-Talk.txt', create_using=nx.DiGraph(), nodetype=int)
 print "Graph Imported, analysing and plotting node degrees."
 
-in_degrees = g.in_degree()
-in_values = sorted(set(in_degrees.values()))
-in_hist = [in_degrees.values().count(x) for x in range(0,11)]
 
-with open("results/in-degree_bottom.txt", "w+") as file:
-	for i in range(0, 11):
-		file.write("Nodes with in-degree of {}: {}\n".format(i, in_hist[i]))
+with open("results/degree_top.txt", "w+") as file:
+    deg = g.in_degree()
+    file.write("Top 100 Nodes by In-degree\n")
+    file.write("Node, In-Degree\n")
+    for n,p in get_top_keys(deg, 100):
+        file.write("{} {}\n".format(n, deg[n]))
+
+    deg = g.out_degree()
+    file.write("\nTop 100 Nodes by Out-degree\n")
+    file.write("Node, Out-Degree\n")
+    for n,p in get_top_keys(deg, 100):
+        file.write("{} {}\n".format(n, deg[n]))   
